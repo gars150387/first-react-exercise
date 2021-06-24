942942dad5976c89c6d759496f4f3bf7ccf597ba
gars150387 <gar.santeliz@gmail.com>
2021-06-24 01:27:15 +0000

    origin

--- exercises/02.2-maping-array-of-objects-to-li/index.js
+++ exercises/02.2-maping-array-of-objects-to-li/index.js
@@ -8,7 +8,13 @@ const planets = ["Mars", "Venus", "Jupiter", "Earth", "Saturn", "Neptune"];
  * planets in html called planetsInHTML
  */
 
+// const planetsInHTML = planets.map(planets => planets, i);
+
+const planetsInHTML = planets.map((singlePlanets, i) => {
+	return <li className="list-group-item">{singlePlanets}</li>;
+});
+
 // 2) add the array planetsInHTML inside the innerHTML of this ul
-const content = (<ul className="list-group m-5"></ul>);
+const content = <ul className="list-group m-5">{planetsInHTML}</ul>;
 
 ReactDOM.render(content, document.querySelector("#myDiv"));
